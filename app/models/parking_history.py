ab5f4393773756a2911d7916b2db1d953893a14b
Kaushik1766 <kaushiksaha004@gmail.com>
2026-01-08 11:09:34 +0530

    parking history

--- app/models/parking_history.py
+++ app/models/parking_history.py
@@ -1,17 +1,14 @@
-# "Numberplate": "UP32KK2222",
-# "BuildingId": "BUILDING#B1",
-# "FloorNumber": 1,
-# "SlotId": 2,
-# "StartTime": 1763572066,
-# "EndTime": 1763572066
 from pydantic.fields import Field
 from pydantic import BaseModel
+import datetime
 
 
 class ParkingHistory(BaseModel):
+    user_id: str = Field(exclude=True)
     numberplate: str = Field(alias="NumberPlate")
     building_id: str = Field(alias="BuildingId")
     floor_number: int = Field(alias="FloorNumber")
     slot_id: int = Field(alias="SlotId")
-    start_time: int = Field(alias="StartTime")
+    start_time: int = Field( default=datetime.datetime.now(tz=datetime.timezone.utc), alias="StartTime")
     end_time: int = Field(alias="EndTime")
+    parking_id: str = Field(alias="ParkingId")
--- app/repository/parking_repo.py
+++ app/repository/parking_repo.py
@@ -1,9 +1,16 @@
+from app.errors.web_exception import DB_ERROR
+from app.errors.web_exception import WebException
+from typing import cast
+import datetime
 from fastapi import Depends
 from mypy_boto3_dynamodb.service_resource import DynamoDBServiceResource
 from typing import Annotated
+from app.models.parking_history import ParkingHistory
+from asyncio import to_thread
 
 from app.constants import TABLE
 from app.dependencies import get_db
+from boto3.dynamodb.conditions import Key
 
 
 class ParkingRepostiory:
@@ -15,4 +22,48 @@ class ParkingRepostiory:
         self.table = db.Table(TABLE)
 
 
-    # async def add_parking(self, ):
+    async def add_parking(self, parking: ParkingHistory):
+        await to_thread(
+            lambda: self.table.put_item(
+                Item={
+                    "PK": f"USER#{parking.user_id}",
+                    "SK": f"PARKING#{parking.start_time}",
+                    **parking.model_dump()
+                }
+            )
+        )
+
+    async def unpark_by_numberplate(self, user_id: str, numberplate: str):
+        parking = await to_thread(
+            lambda: self.table.query(
+                KeyConditionExpression=Key("PK").eq(f"USER#{user_id}")& Key("SK").begins_with("PARKING#"),
+                FilterExpression=Key("NumberPlate").eq(numberplate) & Key("EndTime").not_exists(),
+                Limit=1,
+            ).get("Items")
+        )       
+
+        if not parking:
+            raise WebException(status_code=404, message="No active parking found for the given numberplate", error_code=DB_ERROR)
+
+        parking_sk = parking[0]["SK"]
+        await to_thread(
+            lambda: self.table.update_item(
+                Key={
+                    "PK": f"USER#{user_id}",
+                    "SK": f"PARKING#{parking_sk}",
+                },
+                UpdateExpression="SET EndTime = :end_time",
+                ExpressionAttributeValues={
+                    ":end_time": int(datetime.datetime.now(tz=datetime.timezone.utc).timestamp())
+                }
+            )
+        )
+
+    async def get_parking_history(self, user_id: str, start_time: int, end_time: int) -> list[ParkingHistory]:
+        response = await to_thread(
+            lambda: self.table.query(
+                KeyConditionExpression=Key("PK").eq(f"USER#{user_id}") & Key("SK").between(f"PARKING#{start_time}", f"PARKING#{end_time}"),
+            ).get("Items", [])
+        )
+        
+        return [ParkingHistory(**cast(dict, item)) for item in response]
